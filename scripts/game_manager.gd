146dfb7ecc6d048f16e69984edb9066176fc2b06
Sophie <sophie.thi.nguyen11@gmail.com>
2025-08-24 16:22:44 -0700

    gamemanager actual commit

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,1 +1,10 @@
 extends Node
+
+func _process(delta: float) -> void:
+	checkRespawn()
+
+func checkRespawn():
+	if Input.is_action_just_pressed("reset"):
+		print("resetting game")
+		get_tree().reload_current_scene()
+		Engine.time_scale = 1

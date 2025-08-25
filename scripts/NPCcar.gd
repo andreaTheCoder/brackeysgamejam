d1756440d1ec55bbfc37899f99793ce4180efa77
Sophie <sophie.thi.nguyen11@gmail.com>
2025-08-24 17:12:19 -0700

    tweaked car spawns

--- scripts/NPCcar.gd
+++ scripts/NPCcar.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
-@export var SPEED = -600
+@export var SPEED = 1000
 
 func _process(delta: float) -> void:
-	position.x += SPEED * delta
+	position.x += -SPEED * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	print("this car was hit")
--- scripts/lane.gd
+++ scripts/lane.gd
@@ -5,21 +5,25 @@ extends Node2D
 @onready var parent_lane = get_node("../Lane")
 
 var car_template = preload("res://scenes/NPCcar.tscn")
-const minSpawnTime: float = 2
-const maxSpawnTime: float = 5
+
+# Set possible min/max wait times for cars to spawn
+const minSpawnTime: float = 4
+const maxSpawnTime: float = 7
 
 func _process(delta: float) -> void:
 	pass
+
 func _ready() -> void:
-	spawn_car()
 	spawn_timer.start()
-# instaniates car at spawner node
+
+# Instaniates car at spawner node
 func spawn_car():
 	print("spawning car")
 	var my_car = car_template.instantiate()
 	my_car.position = spawner.position
 	parent_lane.add_child(my_car)
  
+# Spawns new car and restarts timer at random interval
 func _on_spawn_timer_timeout() -> void:
 	spawn_car()
 	spawn_timer.wait_time = randf_range(minSpawnTime, maxSpawnTime)

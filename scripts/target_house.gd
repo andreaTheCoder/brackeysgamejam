0c25adef0a00975d4b3fae870f0d92d6e0aaf836
Jasper-Cn <leshan.jasper@gmail.com>
2025-08-25 17:20:20 -0700

    added house and delivery key
    when bike is at house and delivery key is pressed (either shift), end screen shows up

--- scripts/target_house.gd
+++ scripts/target_house.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 const BIKE = preload("res://scenes/bike.tscn")
-
+var at_house = false
+var game_over_scene:PackedScene = load("res://scenes/end_screen.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,9 +10,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if at_house and Input.is_action_pressed("deliver"):
+		get_tree().change_scene_to_packed(game_over_scene)
 
 func _on_body_entered(body: Node2D) -> void:
-	$"../Bike".velocity.x = 0
-	pass # Replace with function body.
- 
+	at_house = true

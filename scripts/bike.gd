a80426e914691a8ca153092517314fb70041cc92
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2025-08-25 19:18:18 -0700

    backwards movement
    should we add it?

--- scripts/bike.gd
+++ scripts/bike.gd
@@ -3,10 +3,9 @@ extends CharacterBody2D
 # X direction movement
 const X_ACCEL = 1000.0 # X direction acceleration
 const MAX_SPEED = 4000
-const X_DECEL = 1000 # rate at which you decelerate when spacebar is let go
-# y deceleration speed
-const Y_DECEL = 5
-
+const X_BACKWARDS_SLOW = .5
+const DECEL_SPEED = 5
+const BACKWARDS_MAX_SPEED = 2000
 # if car is stunned stunned = 0, meaning that all acceleration is null
 var stunned = false
 const STUN_DURATION = 3
@@ -17,8 +16,10 @@ func _physics_process(delta: float) -> void:
 	# Handle forward movement.
 	if Input.is_action_pressed("forward") and not stunned: 
 		velocity.x += clamp(X_ACCEL*delta, 0, MAX_SPEED) # Add the acceleration to the speed, but only until max speed
+	elif Input.is_action_pressed("backward") and not stunned:
+		velocity.x -= clamp(X_ACCEL*X_BACKWARDS_SLOW*delta, 0, BACKWARDS_MAX_SPEED)
 	else:
-		velocity.x = max(velocity.x - X_DECEL *delta, 0) # Set velocity to the velocity minus the deceleration, but cap at 0
+		velocity.x = move_toward(velocity.x, 0, DECEL_SPEED)
 	
 	# Handle up/down movement
 	if Input.is_action_pressed("up") and not stunned:
@@ -26,7 +27,7 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("down") and not stunned:
 		velocity.y += clamp(X_ACCEL* delta, 0, MAX_SPEED) # Add the acceleration to the speed, but only until max speed
 	else:
-		velocity.y = move_toward(velocity.y, 0, Y_DECEL) # moves velocity towards 0, slowing it down every from by 10 when not pressing up or down
+		velocity.y = move_toward(velocity.y, 0, DECEL_SPEED) # moves velocity towards 0, slowing it down every from by 10 when not pressing up or down
 	
 	move_and_slide()
 

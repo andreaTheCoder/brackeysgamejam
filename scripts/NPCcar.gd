3b94d7d6d563cd1f7086c11d83513d655c6f8612
Sophie <sophie.thi.nguyen11@gmail.com>
2025-08-26 10:11:44 -0700

    added.3 lane

--- scripts/NPCcar.gd
+++ scripts/NPCcar.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
 @export var SPEED = 1000
-@onready var bike: CharacterBody2D = $"../../Bike"
+var direction = -1
+#@onready var bike: CharacterBody2D = $"../../Bike"
+@onready var bike: CharacterBody2D = $"../../%Bike"
 
 
 func _process(delta: float) -> void:
-	position.x += -SPEED * delta
+	position.x += -SPEED * delta * direction
 
 func _on_body_entered(body: Node2D) -> void:
 	print("this car was stunned")

af8ec930a9510bfccce0c3ed9f03a10e27b3ddec
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2025-08-27 20:17:26 -0700

    fix weird car following player problem

--- scripts/NPCcar.gd
+++ scripts/NPCcar.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 const SPEED = 20
 var direction = -1
-@onready var bike: CharacterBody2D = $"../../../Bike"
+@onready var bike: CharacterBody2D = $"../Bike"
 
 func _process(delta: float) -> void:
 	position.x += SPEED * delta * direction
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -8,6 +8,7 @@ var car_template = preload("res://scenes/NPCcar.tscn")
 # Set possible min/max wait times for cars to spawn
 const minSpawnTime: float = 1
 const maxSpawnTime: float = 5
+const STARTING_BEHIND_X = 50 # x that the car spawns behind the player
 
 
 func _ready() -> void:
@@ -17,8 +18,9 @@ func _ready() -> void:
 func spawn_car():
 	print("spawning car")
 	var my_car = car_template.instantiate()
-	my_car.position = position
-	add_child(my_car)
+	my_car.position.x = get_parent().position.x - STARTING_BEHIND_X
+	my_car.position.y = position.y
+	get_parent().get_parent().add_child(my_car)
 	my_car.direction = spawn_direction
  
 # Spawns new car and restarts timer at random interval

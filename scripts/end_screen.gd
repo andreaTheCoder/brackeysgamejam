686de7c00bcdaaeadc8003aeb3865d30bb2dc2a2
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2025-08-27 17:48:35 -0700

    fixed it so button is not created by script

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -2,19 +2,12 @@ extends Node2D
 
 const GAME = preload("res://scenes/game.tscn")
 var game_scene:PackedScene = load("res://scenes/game.tscn")
+@onready var button: Button = $Control/Button
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var dynamic_font = FontFile.new()
-	dynamic_font.font_data = load("res://assets/brackeys_platformer_assets 2/fonts/PixelOperator8-Bold.ttf")
-	var button = Button.new()
-	button.set_position(Vector2(1000, 1300))
-	button.size = Vector2(1000, 100)
-	button.text = "Next delivery"
-	button.add_theme_font_override("font", dynamic_font)
 	button.pressed.connect(_button_pressed)
-	add_child(button)
 
 func _button_pressed():
 	print("Next delivery!")

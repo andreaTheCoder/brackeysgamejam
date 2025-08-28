4be9f8788783fd74216665260542e7b89b8279e0
Jasper-Cn <leshan.jasper@gmail.com>
2025-08-27 20:37:19 -0700

    Fixed minor bugs and made my ocd happy

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -7,11 +7,12 @@ var car_template = preload("res://scenes/NPCcar.tscn")
 
 # Set possible min/max wait times for cars to spawn
 const minSpawnTime: float = 1
-const maxSpawnTime: float = 5
-const STARTING_BEHIND_X = 50 # x that the car spawns behind the player
+const maxSpawnTime: float = 11
+const STARTING_BEHIND_X = 100 # x that the car spawns behind the player
 
 
 func _ready() -> void:
+	spawn_timer.wait_time = randf_range(minSpawnTime, maxSpawnTime)
 	spawn_timer.start()
 
 # Instaniates car at spawner node

66216d98039f5e6675545c4e88e47c871893ac6a
Jasper-Cn <leshan.jasper@gmail.com>
2025-08-27 19:49:17 -0700

    minor bug fixes

--- scripts/Chunks.gd
+++ scripts/Chunks.gd
@@ -4,8 +4,7 @@ extends Area2D
 var chunk_template = preload("res://scenes/lane.tscn")
 var chunk_num = 1
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	var chunk = chunk_template.instantiate()
 	chunk.position.x = chunk_num * 256
 	call_deferred("add_child", chunk)
-	

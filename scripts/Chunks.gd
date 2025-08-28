b2e33775ef169ecd2c1aed84720444ef36e706d4
Jasper-Cn <leshan.jasper@gmail.com>
2025-08-27 21:07:32 -0700

    Changed collision sizes and fixed infinite speed glitches

--- scripts/Chunks.gd
+++ scripts/Chunks.gd
@@ -2,9 +2,8 @@ extends Area2D
 
 @onready var game: Node2D = $"."
 var chunk_template = preload("res://scenes/chunk.tscn")
-var chunk_num = 1
 
 func _on_body_entered(_body: Node2D) -> void:
 	var chunk = chunk_template.instantiate()
-	chunk.position.x = chunk_num * 256
+	chunk.position.x = 256
 	get_parent().call_deferred("add_child", chunk)
--- scripts/bike.gd
+++ scripts/bike.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 # X direction movement
 const X_BACKWARDS_SLOW = .5
 const DECEL_SPEED = 5
-const BACKWARDS_MAX_SPEED = 2000# rate at which you decelerate when spacebar is let go
+const BACKWARDS_MAX_SPEED = -200# rate at which you decelerate when spacebar is let go
 const X_ACCEL = 100.0 # X direction acceleration
 const MAX_SPEED = 400
 const X_DECEL = 100 # rate at which you decelerate when spacebar is let go
@@ -18,17 +18,17 @@ const STUN_DURATION = 3
 
 func _physics_process(delta: float) -> void:
 	# Handle forward movement.
-	if Input.is_action_pressed("forward") and not stunned: 
-		velocity.x += clamp(X_ACCEL*delta, 0, MAX_SPEED) # Add the acceleration to the speed, but only until max speed
+	if Input.is_action_pressed("forward") and not stunned:
+		velocity.x = clamp(velocity.x + X_ACCEL * delta, BACKWARDS_MAX_SPEED, MAX_SPEED) # Add the acceleration to the speed, but only until max speed
 	elif Input.is_action_pressed("backward") and not stunned:
-		velocity.x -= clamp(X_ACCEL*X_BACKWARDS_SLOW*delta, 0, BACKWARDS_MAX_SPEED)
+		velocity.x = clamp(velocity.x - X_ACCEL * X_BACKWARDS_SLOW * delta, BACKWARDS_MAX_SPEED, MAX_SPEED)
 	else:
 		velocity.x = move_toward(velocity.x, 0, DECEL_SPEED)
 	# Handle up/down movement
 	if Input.is_action_pressed("up") and not stunned:
-		velocity.y -= clamp(X_ACCEL * delta, 0, MAX_SPEED) # Add the acceleration to the speed, but only until max speed
+		velocity.y = clamp(velocity.y - X_ACCEL * delta, BACKWARDS_MAX_SPEED, MAX_SPEED) # Add the acceleration to the speed, but only until max speed
 	elif Input.is_action_pressed("down") and not stunned:
-		velocity.y += clamp(X_ACCEL* delta, 0, MAX_SPEED) # Add the acceleration to the speed, but only until max speed
+		velocity.y = clamp(velocity.y + X_ACCEL * delta, BACKWARDS_MAX_SPEED, MAX_SPEED) # Add the acceleration to the speed, but only until max speed
 	else:
 		velocity.y = move_toward(velocity.y, 0, Y_DECEL) # moves velocity towards 0, slowing it down every from by 10 when not pressing up or down
 	

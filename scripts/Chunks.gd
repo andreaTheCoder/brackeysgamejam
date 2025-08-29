a2e4ebe12ff0ffc252289db3fc5420c9af988d75
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2025-08-28 20:07:02 -0700

    fix chunk spawning distance

--- scripts/Chunks.gd
+++ scripts/Chunks.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 @onready var game: Node2D = $"."
 var chunk_template = preload("res://scenes/chunk.tscn")
-
+var row = 1
+const DISTANCE = 144
 func _on_body_entered(_body: Node2D) -> void:
 	var chunk = chunk_template.instantiate()
-	chunk.position.x = 64
+	chunk.position.x = row*DISTANCE
+	row += 1
 	get_parent().call_deferred("add_child", chunk)

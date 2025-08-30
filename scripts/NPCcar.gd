6b193075a9c5092abb3168db93e2f193acc07595
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2025-08-29 22:18:09 -0700

    cars now spawn in front

--- scripts/NPCcar.gd
+++ scripts/NPCcar.gd
@@ -1,11 +1,11 @@
 extends Area2D
-
+var front
 const SPEED = 75
 var direction = -1
 @onready var bike: CharacterBody2D = $"../Bike"
 
 func _process(delta: float) -> void:
 	position.x += SPEED * delta * direction
-
+	
 func _on_body_entered(_body: Node2D) -> void:
 	bike.stun()
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,30 +1,40 @@
 extends Node2D
 
-@export var spawn_direction = 1
+@export var front = true
 @onready var spawn_timer: Timer = $SpawnTimer
 
 var car_template = preload("res://scenes/NPCcar.tscn")
 
 # Set possible min/max wait times for cars to spawn
-const minSpawnTime: float = 1
+const minSpawnTime: float = 5
 const maxSpawnTime: float = 9
 const STARTING_BEHIND_X = 100 # x that the car spawns behind the player
 
 
 func _ready() -> void:
-	spawn_timer.wait_time = randf_range(minSpawnTime, maxSpawnTime)
+	if front:
+		spawn_timer.wait_time = randf_range(minSpawnTime, maxSpawnTime*.5)
+	else:
+		spawn_timer.wait_time = randf_range(minSpawnTime, maxSpawnTime)
 	spawn_timer.start()
 
 # Instaniates car at spawner node
 func spawn_car():
 	var my_car = car_template.instantiate()
-	my_car.position.x = get_parent().position.x - STARTING_BEHIND_X
+	if front:
+		my_car.position.x = get_parent().get_parent().position.x + STARTING_BEHIND_X
+	else:
+		my_car.position.x = get_parent().get_parent().position.x - STARTING_BEHIND_X
 	my_car.position.y = position.y
-	get_parent().get_parent().add_child(my_car)
-	my_car.direction = spawn_direction
+	# family tree be like
+	get_parent().get_parent().get_parent().add_child(my_car)
+	my_car.direction = 1
  
 # Spawns new car and restarts timer at random interval
 func _on_spawn_timer_timeout() -> void:
 	spawn_car()
-	spawn_timer.wait_time = randf_range(minSpawnTime, maxSpawnTime)
+	if front:
+		spawn_timer.wait_time = randf_range(minSpawnTime, maxSpawnTime*.5)
+	else:
+		spawn_timer.wait_time = randf_range(minSpawnTime, maxSpawnTime)
 	spawn_timer.start()
